63aba91f290f338f2bcc1a321d6d8cd28554a14e
shevavm <shevavm@gmail.com>
2020-10-28 09:33:53 +0200

    new Game add it to anonim function

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -2,10 +2,12 @@
 fun main(args: Array<String>) {
     //5.2 println({
     //5.3 val greetingFunction: ()->String= {
-    val greetingFunction: (String)->String= {playerName ->
+    //5.4 val greetingFunction: (String)->String= {playerName ->
+    val greetingFunction: (String)->String= {
         val currentYear = 2018
         //5.3 "Welcome to SimVillage, Mayor! (copiright $currentYear)"
-        "Welcome to SimVillage, $playerName! (copiright $currentYear)"
+        //5.4 "Welcome to SimVillage, $playerName! (copiright $currentYear)"
+        "Welcome to SimVillage, $it! (copiright $currentYear)"
     }//5.2 }())
     //5.3 "Guyal"
     println(greetingFunction("Guyal"))
