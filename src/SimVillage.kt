dad08c6f85d3417da59cd6e5ef076e5b58f9b364
shevavm <shevavm@gmail.com>
2020-10-28 15:04:50 +0200

    SimVillage add automatic type detection in greetingFunction

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -4,15 +4,21 @@ fun main(args: Array<String>) {
     //5.3 val greetingFunction: ()->String= {
     //5.4 val greetingFunction: (String)->String= {playerName ->
     //5.5 val greetingFunction: (String)->String= {
-    val greetingFunction: (String, Int)->String= {playerName, numBuildings ->
+    //5.6 val greetingFunction: (String, Int)->String= {playerName, numBuildings ->
+    val greetingFunction = {playerName: String, numBuildings: Int ->
         val currentYear = 2018
         //5.3 "Welcome to SimVillage, Mayor! (copiright $currentYear)"
         //5.4 "Welcome to SimVillage, $playerName! (copiright $currentYear)"
         //5.5 "Welcome to SimVillage, $it! (copiright $currentYear)"
-        println("Adding $numBuildings houses`")
+        println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copiright $currentYear)"
     }//5.2 }())
     //5.3 "Guyal"
     //5.5 println(greetingFunction("Guyal"))
-    println(greetingFunction("Guyal", 2))
+    //5.7 println(greetingFunction("Guyal", 2))
+    runSimulation ("Guyal", greetingFunction)
+}
+fun runSimulation (playerName: String, greetingFunction: (String, Int) -> String) {
+    val numBuildings = (1..3).shuffled().last()
+    println(greetingFunction(playerName, numBuildings))
 }
