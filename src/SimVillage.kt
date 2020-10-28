29844a921b50d306e3b2dce2f926866ecaeff4c2
shevavm <shevavm@gmail.com>
2020-10-28 10:42:55 +0200

    new Game add the second argument of the anonymous function

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -3,12 +3,16 @@ fun main(args: Array<String>) {
     //5.2 println({
     //5.3 val greetingFunction: ()->String= {
     //5.4 val greetingFunction: (String)->String= {playerName ->
-    val greetingFunction: (String)->String= {
+    //5.5 val greetingFunction: (String)->String= {
+    val greetingFunction: (String, Int)->String= {playerName, numBuildings ->
         val currentYear = 2018
         //5.3 "Welcome to SimVillage, Mayor! (copiright $currentYear)"
         //5.4 "Welcome to SimVillage, $playerName! (copiright $currentYear)"
-        "Welcome to SimVillage, $it! (copiright $currentYear)"
+        //5.5 "Welcome to SimVillage, $it! (copiright $currentYear)"
+        println("Adding $numBuildings houses`")
+        "Welcome to SimVillage, $playerName! (copiright $currentYear)"
     }//5.2 }())
     //5.3 "Guyal"
-    println(greetingFunction("Guyal"))
+    //5.5 println(greetingFunction("Guyal"))
+    println(greetingFunction("Guyal", 2))
 }
