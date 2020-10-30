522a07fb183e346ae50e2ca8e46dda33f40f1272
shevavm <shevavm@gmail.com>
2020-10-30 23:00:55 +0200

    SimVillage add runSimulation + lamda. 5.8

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -5,7 +5,9 @@ fun main(args: Array<String>) {
     //5.4 val greetingFunction: (String)->String= {playerName ->
     //5.5 val greetingFunction: (String)->String= {
     //5.6 val greetingFunction: (String, Int)->String= {playerName, numBuildings ->
-    val greetingFunction = {playerName: String, numBuildings: Int ->
+    //5.8
+    //val greetingFunction = {playerName: String, numBuildings: Int ->
+    runSimulation("Guyal") { playerName, numBuildings ->
         val currentYear = 2018
         //5.3 "Welcome to SimVillage, Mayor! (copiright $currentYear)"
         //5.4 "Welcome to SimVillage, $playerName! (copiright $currentYear)"
@@ -16,9 +18,10 @@ fun main(args: Array<String>) {
     //5.3 "Guyal"
     //5.5 println(greetingFunction("Guyal"))
     //5.7 println(greetingFunction("Guyal", 2))
-    runSimulation ("Guyal", greetingFunction)
+
+    //5.8 ? runSimulation ("Guyal", greetingFunction)
 }
 fun runSimulation (playerName: String, greetingFunction: (String, Int) -> String) {
-    val numBuildings = (1..3).shuffled().last()
+    val numBuildings = (1..3).shuffled().last() //випадковий вибір 1,2 або 3
     println(greetingFunction(playerName, numBuildings))
 }
